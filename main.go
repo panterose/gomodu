be1218720a5c2086586ca3a1c6e1493d263883dc
benoi <benoit.leprevost@gmail.com>
2019-10-27 21:18:27 +0000

    now with 3 coroutine

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -20,27 +22,45 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 	encoder := json.NewEncoder(w)
 	tasks := make(chan task)
 
-	//json.NewEncoder(w).Encode(newTask)
-	go createTask(tasks)
+	// we have 3 goroutines, we need a WaitGroup to orchestrate the work
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	// fork the work in parallel
+	go createTask(tasks, "sourceA", &wg)
+	go createTask(tasks, "sourceB", &wg)
+	go createTask(tasks, "sourceC", &wg)
+
+	// cleanup goroutine
+	go func() {
+		wg.Wait()
+		fmt.Println("All produced: let's close")
+		close(tasks)
+	}()
+
+	// this create the JSON in parallel
 	for t := range tasks {
-		encoder.Encode(t)
+		err := encoder.Encode(t)
+		if err != nil {
+			fmt.Println("Something wrong happened")
+		}
 	}
 	//w.WriteHeader(http.StatusOK)
 }
 
-func createTask(tasks chan task) {
+func createTask(tasks chan task, source string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := 1; i <= 10; i++ {
 		time.Sleep(100 * time.Millisecond)
 		id := strconv.Itoa(i)
 		task := task{
-			ID:          id,
-			Name:        "Name" + id,
+			ID:          source + "_" + id,
+			Name:        "Name" + "_" + id,
 			Description: "Does't matter",
 		}
+		fmt.Println("Produced Task: " + task.ID)
 		tasks <- task
 	}
-	close(tasks)
-	//json.NewEncoder(w).Encode(newTask)
 }
 
 type task struct {
