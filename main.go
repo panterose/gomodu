9c89c9e1321cf6cab78573ee058ea0f00254a37e
benoi <benoit.leprevost@gmail.com>
2019-10-27 21:03:51 +0000

    now using goroutine

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
-}
-
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/tasks", taskHandler)
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func taskHandler(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	tasks := make(chan task)
+
+	//json.NewEncoder(w).Encode(newTask)
+	go createTask(tasks)
+	for t := range tasks {
+		encoder.Encode(t)
+	}
+	//w.WriteHeader(http.StatusOK)
+}
+
+func createTask(tasks chan task) {
+	for i := 1; i <= 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		id := strconv.Itoa(i)
+		task := task{
+			ID:          id,
+			Name:        "Name" + id,
+			Description: "Does't matter",
+		}
+		tasks <- task
+	}
+	close(tasks)
+	//json.NewEncoder(w).Encode(newTask)
+}
+
+type task struct {
+	ID          string `json:"ID"`
+	Name        string `json:"Name"`
+	Description string `json:"Description"`
 }
