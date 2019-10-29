0de9d994a85340d58aab900f8dd76b401316ff91
benoi <benoit.leprevost@gmail.com>
2019-10-29 22:40:58 +0000

    now calling other endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/panterose/gomodu
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/pkg/profile v1.3.0
+)
--- main.go
+++ main.go
@@ -3,23 +3,81 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/pkg/profile"
 )
 
+const port = 8080
+const randTask = 50
+
 func main() {
+	defer profile.Start().Stop()
+
+	f, err := os.OpenFile("gomodu.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetPrefix("LOG: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+	log.SetOutput(f)
+	log.Println("init started")
+
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/tasks", taskHandler)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/agg", aggregateHandler)
+	router.HandleFunc("/task/{source}", taskHandler)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	vars := mux.Vars(r)
+	source := vars["source"]
+
+	taskDuration := time.Duration(rand.Intn(10)+10) * time.Millisecond
+	nbTasks := rand.Intn(randTask) + randTask
+	tasks := make([]task, nbTasks)
+	for i := 0; i < nbTasks; i++ {
+		time.Sleep(taskDuration)
+		id := strconv.Itoa(i)
+		task := task{
+			ID:          source + "_" + id,
+			Name:        "Name" + "_" + id,
+			Description: "Does't matter",
+		}
+		log.Printf("%s Produced Task: %s in %s \n", source, task.ID, taskDuration)
+		tasks[i] = task
+	}
+	createSlice := time.Since(start)
+	err := json.NewEncoder(w).Encode(tasks)
+	if err != nil {
+		log.Println("error:", err)
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+	//w.WriteHeader(http.StatusOK)
+
+	total := time.Since(start)
+	log.Printf("%s finished to produced %d tasks in %s/%s \n", source, nbTasks, createSlice, total)
+}
+
+func aggregateHandler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+
 	encoder := json.NewEncoder(w)
 	tasks := make(chan task)
 
@@ -28,16 +86,16 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 	wg.Add(3)
 
 	// fork the work in parallel
-	go createTask("sourceA", tasks, &wg)
-	go createTask("sourceB", tasks, &wg)
-	go createTask("sourceC", tasks, &wg)
+	go readTask("sourceA", tasks, &wg)
+	go readTask("sourceB", tasks, &wg)
+	go readTask("sourceC", tasks, &wg)
 
 	// cleanup goroutine
 	go func() {
 		start := time.Now()
 		wg.Wait()
 		elapsed := time.Since(start)
-		fmt.Printf("All produced: let's close, done in %s", elapsed)
+		log.Printf("All produced: let's close, done in %s \n", elapsed)
 		close(tasks)
 	}()
 
@@ -45,26 +103,38 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 	for t := range tasks {
 		err := encoder.Encode(t)
 		if err != nil {
-			fmt.Printf("Something wrong happened")
+			log.Println("error:", err)
+			http.Error(w, "internal error", http.StatusInternalServerError)
+			return
 		}
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	elapsed := time.Since(start)
+	log.Println("Finished to aggregated in", elapsed)
 }
 
-func createTask(source string, tasks chan task, wg *sync.WaitGroup) {
+func readTask(source string, tasks chan task, wg *sync.WaitGroup) {
 	defer wg.Done()
-	taskDuration := time.Duration(rand.Intn(10)+10) * time.Millisecond
-	nbTasks := rand.Intn(50) + 50
-	for i := 1; i <= nbTasks; i++ {
-		time.Sleep(taskDuration)
-		id := strconv.Itoa(i)
-		task := task{
-			ID:          source + "_" + id,
-			Name:        "Name" + "_" + id,
-			Description: "Does't matter",
-		}
-		fmt.Printf("%s Produced Task: %s in %s \n", source, task.ID, taskDuration)
+	res, err := http.Get("http://localhost:" + strconv.Itoa(port) + "/task/" + source)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	var newTasks []task
+	jsonErr := json.Unmarshal(body, &newTasks)
+	if jsonErr != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	for _, task := range newTasks {
 		tasks <- task
 	}
 }
